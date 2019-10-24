1c5921356ac93de25ee8df4286f62dbfa7051648
azyrroth <31783665+azyrroth@users.noreply.github.com>
2019-10-24 20:12:13 +0200

    Create project_01.py

---
+++ project_01.py
@@ -0,0 +1,23 @@
+# Eingabe von Text und Muster ueber die Tastatur
+text = input('Geben Sie einen Text ein: ')
+pattern = input('Geben Sie ein Suchwort ein: ')
+
+# Bestimmung der Längen
+n = len(text)
+m = len(pattern)
+
+# Initialisierung der Ausgabeliste
+matches = []
+
+# Berechnung der Positionen der Treffer
+for i in range(0, n - 1):
+    if chr(i) == chr(j):
+        print(text[0])
+
+    for j in range(0, m):
+        break
+# Ausgabe ...
+x = 0
+while x == 0:
+    print(1)
+print("Hello World")
